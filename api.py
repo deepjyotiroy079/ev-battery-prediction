200bb7cf06226d2ebbba48176c6ea668c7029bff
Deepjyoti Roy <deepjyotiroy079@gmail.com>
2026-06-19 15:51:42 +0530

    Initial commit 3

--- api.py
+++ api.py
@@ -168,7 +168,7 @@ def predict(payload: BatteryTelemetry):
     )
 
 
-# if __name__ == "__main__":
-#     import uvicorn
+if __name__ == "__main__":
+    import uvicorn
 
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
